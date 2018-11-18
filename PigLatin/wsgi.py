c53266a2f801373d34e23451792b2da488bc08a0
Seda Vardanyan <sedavardanyan5@gmail.com>
2018-11-18 17:20:56 +0400

    settings

--- PigLatin/wsgi.py
+++ PigLatin/wsgi.py
@@ -1,16 +1,8 @@
-"""
-WSGI config for PigLatin project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/1.11/howto/deployment/wsgi/
-"""
-
 import os
-
 from django.core.wsgi import get_wsgi_application
+from whitenoise.django import DjangoWhiteNoise
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "PigLatin.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "bootcamp.settings")
 
 application = get_wsgi_application()
+application = DjangoWhiteNoise(application)
---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn PigLatin.wsgi --log-file -
